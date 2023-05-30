0567cfc9f9e9675caff16d3bd14ec3a427c973df
Kartik1213 <38133941+Kartik1213@users.noreply.github.com>
2023-05-31 02:43:39 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+require("./config/database").connect();
+const express = require("express");
+const responseHandler = require('./response-handler.js')
+const app = express();
+const profileRouter = require("./routes/profileRoutes");
+const articleRouter = require("./routes/articleRoutes");
+
+app.use(express.json());
+app.use("/api", profileRouter);
+app.use("/api", articleRouter);
+ 
+
+ 
+module.exports = app;
